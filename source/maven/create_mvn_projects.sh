c5600f5fe66aada9d2225069fa0d9bf5a0f607e2
nadirsaghar <nadirsaghar@emory.edu>
2012-11-12 15:56:47 -0500

    1. Refactored junit projects 2. Added maved build project

---
+++ source/maven/create_mvn_projects.sh
@@ -0,0 +1,14 @@
+mvn archetype:generate   -DinteractiveMode=false -DarchetypeArtifactId=maven-archetype-quickstart   -DgroupId=bindaas   -DartifactId=bindaas-core-api
+mvn archetype:generate   -DinteractiveMode=false -DarchetypeArtifactId=maven-archetype-quickstart   -DgroupId=bindaas   -DartifactId=bindaas-core-impl
+mvn archetype:generate   -DinteractiveMode=false -DarchetypeArtifactId=maven-archetype-quickstart   -DgroupId=bindaas   -DartifactId=bindaas-installer-updater
+mvn archetype:generate   -DinteractiveMode=false -DarchetypeArtifactId=maven-archetype-quickstart   -DgroupId=bindaas   -DartifactId=bindaas-security-api
+mvn archetype:generate   -DinteractiveMode=false -DarchetypeArtifactId=maven-archetype-quickstart   -DgroupId=bindaas   -DartifactId=bindaas.log4j.config
+mvn archetype:generate   -DinteractiveMode=false -DarchetypeArtifactId=maven-archetype-quickstart   -DgroupId=bindaas   -DartifactId=ldap-bindaas-authentication-provider
+mvn archetype:generate   -DinteractiveMode=false -DarchetypeArtifactId=maven-archetype-quickstart   -DgroupId=bindaas   -DartifactId=db2-datasource-provider
+mvn archetype:generate   -DinteractiveMode=false -DarchetypeArtifactId=maven-archetype-quickstart   -DgroupId=bindaas   -DartifactId=aime-bindaas-datasource-provider-1
+mvn archetype:generate   -DinteractiveMode=false -DarchetypeArtifactId=maven-archetype-quickstart   -DgroupId=bindaas   -DartifactId=generic-sql-datasource-provider
+mvn archetype:generate   -DinteractiveMode=false -DarchetypeArtifactId=maven-archetype-quickstart   -DgroupId=bindaas   -DartifactId=mongodb-datasource-provider
+mvn archetype:generate   -DinteractiveMode=false -DarchetypeArtifactId=maven-archetype-quickstart   -DgroupId=bindaas   -DartifactId=mysql-datasource-provider
+mvn archetype:generate   -DinteractiveMode=false -DarchetypeArtifactId=maven-archetype-quickstart   -DgroupId=bindaas   -DartifactId=junit-aime-bindaas-datasource-provider-1
+mvn archetype:generate   -DinteractiveMode=false -DarchetypeArtifactId=maven-archetype-quickstart   -DgroupId=bindaas   -DartifactId=junit-mongodb-datasource-provider
+mvn archetype:generate   -DinteractiveMode=false -DarchetypeArtifactId=maven-archetype-quickstart   -DgroupId=bindaas   -DartifactId=junit-bindaas-core
