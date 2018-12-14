7c0b21f87e3f91d7b77ef6fb843f88df37624f61
Pradeeban <kk.pradeeban@gmail.com>
2018-12-14 15:57:29 -0500

    Fix docker run

--- distribution/build-extras/bin/docker-run.sh
+++ distribution/build-extras/bin/docker-run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+mkdir bindaas-dist-3.3.2
+
+cd bindaas-dist-3.3.2
+
 wget https://github.com/sharmalab/bindaas/releases/download/v3.3.2/bindaas-dist-3.3.2.tar.gz
 
 tar -xvf bindaas-dist-3.3.2.tar.gz && rm bindaas-dist-3.3.2.tar.gz
 
-cd bindaas-dist-3.3.2
 
 docker stop bindaas-public
 
 docker rm bindaas-public
 
-docker run --name bindaas-public -v bin:/root/bindaas/bin/ bundles:/root/bindaas/bundles/ log:/root/bindaas/log/ -p 8080:8080 -p 9099:9099 pradeeban/bindaas:3.3.2
+docker run --name bindaas-public -v bin:/root/bindaas/bin/ -v bundles:/root/bindaas/bundles/ -v log:/root/bindaas/log/ -p 8080:8080 -p 9099:9099 pradeeban/bindaas:3.3.2
