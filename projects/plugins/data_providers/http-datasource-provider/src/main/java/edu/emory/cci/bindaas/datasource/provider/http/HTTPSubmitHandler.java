c20862a6c33967ba5523c655cfba2ac91a8a6cec
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2019-06-06 16:40:45 -0400

    Add submit csv files

--- projects/plugins/data_providers/http-datasource-provider/src/main/java/edu/emory/cci/bindaas/datasource/provider/http/HTTPSubmitHandler.java
+++ projects/plugins/data_providers/http-datasource-provider/src/main/java/edu/emory/cci/bindaas/datasource/provider/http/HTTPSubmitHandler.java
@@ -1,7 +1,6 @@
 package edu.emory.cci.bindaas.datasource.provider.http;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 import com.google.gson.JsonObject;
 
@@ -10,46 +9,62 @@ import edu.emory.cci.bindaas.framework.model.QueryResult;
 import edu.emory.cci.bindaas.framework.model.RequestContext;
 import edu.emory.cci.bindaas.framework.model.SubmitEndpoint;
 import edu.emory.cci.bindaas.framework.provider.exception.AbstractHttpCodeException;
-import edu.emory.cci.bindaas.framework.provider.exception.MethodNotImplementedException;
 import edu.emory.cci.bindaas.framework.provider.exception.SubmitExecutionFailedException;
 import edu.emory.cci.bindaas.framework.util.IOUtils;
+import edu.emory.cci.bindaas.framework.util.StandardMimeType;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 public class HTTPSubmitHandler implements ISubmitHandler {
 
-	private Log log = LogFactory.getLog(getClass());
+    private Log log = LogFactory.getLog(getClass());
 
-	@Override
-	public QueryResult submit(JsonObject dataSource,
-			JsonObject endpointProperties, InputStream is, RequestContext requestContext)
-			throws AbstractHttpCodeException {
+    @Override
+    public QueryResult submit(JsonObject dataSource,
+                              JsonObject endpointProperties, InputStream is, RequestContext requestContext)
+            throws AbstractHttpCodeException {
 
-		try {
-			String data = IOUtils.toString(is);
-			return submit(dataSource, endpointProperties, data , requestContext);
-		} catch (IOException e) {
-			log.error(e);
-			throw new SubmitExecutionFailedException(HTTPProvider.class.getName() , HTTPProvider.VERSION ,e);
-		}	}
+        try {
+            String data = IOUtils.toString(is);
+            return submit(dataSource, endpointProperties, data, requestContext);
+        } catch (IOException e) {
+            log.error(e);
+            throw new SubmitExecutionFailedException(HTTPProvider.class.getName(), HTTPProvider.VERSION, e);
+        }
+    }
 
-	@Override
-	public QueryResult submit(JsonObject dataSource,
-			JsonObject endpointProperties, String data, RequestContext requestContext)
-			throws AbstractHttpCodeException {
-		throw new MethodNotImplementedException(HTTPProvider.class.getName(), HTTPProvider.VERSION, "Method not implemented");
-	}
+    @Override
+    public QueryResult submit(JsonObject dataSource,
+                       JsonObject endpointProperties, String data, RequestContext requestContext)
+            throws AbstractHttpCodeException {
+        QueryResult queryResult = new QueryResult();
 
-	@Override
-	public SubmitEndpoint validateAndInitializeSubmitEndpoint(
-			SubmitEndpoint submitEndpoint) throws AbstractHttpCodeException {
-		throw new MethodNotImplementedException(HTTPProvider.class.getName(), HTTPProvider.VERSION, "Method not implemented");
-	}
+        try {
+            OutputStream outStream = new FileOutputStream("output.csv");
+            outStream.write(data.getBytes());
 
-	@Override
-	public JsonObject getSubmitPropertiesSchema() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+            queryResult.setData(new ByteArrayInputStream(data.getBytes()));
+            queryResult.setMimeType(StandardMimeType.CSV.toString());
+
+        } catch (IOException e) {
+            log.error(e);
+            throw new SubmitExecutionFailedException(HTTPProvider.class.getName(), HTTPProvider.VERSION, e);
+        }
+
+        return queryResult;
+    }
+
+    @Override
+    public SubmitEndpoint validateAndInitializeSubmitEndpoint(
+            SubmitEndpoint submitEndpoint) throws AbstractHttpCodeException {
+        submitEndpoint.setType(SubmitEndpoint.Type.MULTIPART);
+        return submitEndpoint;
+    }
+
+    @Override
+    public JsonObject getSubmitPropertiesSchema() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
