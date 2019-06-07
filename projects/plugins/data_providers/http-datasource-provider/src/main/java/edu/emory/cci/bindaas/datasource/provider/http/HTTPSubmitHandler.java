937cd1335d6912b243a3573e2991df8262f959ed
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2019-06-07 15:59:28 -0400

    Add HTTP Submit Handler

--- projects/plugins/data_providers/http-datasource-provider/src/main/java/edu/emory/cci/bindaas/datasource/provider/http/HTTPSubmitHandler.java
+++ projects/plugins/data_providers/http-datasource-provider/src/main/java/edu/emory/cci/bindaas/datasource/provider/http/HTTPSubmitHandler.java
@@ -1,25 +1,35 @@
 package edu.emory.cci.bindaas.datasource.provider.http;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Properties;
 
 import com.google.gson.JsonObject;
 
+import edu.emory.cci.bindaas.datasource.provider.http.model.HTTPQuery;
 import edu.emory.cci.bindaas.datasource.provider.http.model.SubmitEndpointProperties;
 import edu.emory.cci.bindaas.framework.api.ISubmitHandler;
 import edu.emory.cci.bindaas.framework.model.QueryResult;
 import edu.emory.cci.bindaas.framework.model.RequestContext;
 import edu.emory.cci.bindaas.framework.model.SubmitEndpoint;
 import edu.emory.cci.bindaas.framework.provider.exception.AbstractHttpCodeException;
+import edu.emory.cci.bindaas.framework.provider.exception.NetworkConnectionException;
+import edu.emory.cci.bindaas.framework.provider.exception.QueryExecutionFailedException;
 import edu.emory.cci.bindaas.framework.provider.exception.SubmitExecutionFailedException;
 import edu.emory.cci.bindaas.framework.util.GSONUtil;
 import edu.emory.cci.bindaas.framework.util.IOUtils;
 import edu.emory.cci.bindaas.framework.util.StandardMimeType;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.impl.client.HttpClients;
 
 public class HTTPSubmitHandler implements ISubmitHandler {
 
@@ -43,7 +53,7 @@ public class HTTPSubmitHandler implements ISubmitHandler {
     public QueryResult submit(JsonObject dataSource,
                        JsonObject endpointProperties, String data, RequestContext requestContext)
             throws AbstractHttpCodeException {
-        QueryResult queryResult = new QueryResult();
+        QueryResult result = new QueryResult();
 
         try {
 
@@ -51,19 +61,58 @@ public class HTTPSubmitHandler implements ISubmitHandler {
                     .getGSONInstance().fromJson(endpointProperties,
                             SubmitEndpointProperties.class);
 
+            String submitUrl = submitEndpointProperties.getUrl();
+
             OutputStream outStream = new FileOutputStream(submitEndpointProperties.getOutputfileName());
-            log.info(submitEndpointProperties.getUrl());
+
             outStream.write(data.getBytes());
 
-            queryResult.setData(new ByteArrayInputStream(data.getBytes()));
-            queryResult.setMimeType(StandardMimeType.CSV.toString());
+            HttpClient httpclient = HttpClients.createDefault();
+            HttpPost httppost = new HttpPost(submitUrl);
+            httppost.setEntity(new FileEntity(new File("C:/Users/Marjie/Downloads/twisted_DNA_bundles/monotwist.L1.v1.json")));
+
+            final HttpResponse response = httpclient.execute(httppost);
+
+            if(response!=null && response.getStatusLine().getStatusCode() == 200 && response.getEntity()!=null)
+            {
+
+                result.setCallback(new QueryResult.Callback() {
+
+                    @Override
+                    public void callback(OutputStream servletOutputStream, Properties context)
+                            throws AbstractHttpCodeException {
+
+                        try {
+                            response.getEntity().writeTo(servletOutputStream);
+                        } catch (IOException e) {
+                            log.error(e);
+                            throw new NetworkConnectionException(getClass().getName(), 1 , e);
+                        }
+                    }
+                });
+
+                result.setMimeType(response.getFirstHeader("Content-Type").getValue());
+
+            }
+            else
+            {
+                throw new Exception("Error connecting remote URL [" + submitUrl + "]. Server Response [" + response.getStatusLine().toString() + "]" );
+
+            }
+
+
+//            queryResult.setData(new ByteArrayInputStream(data.getBytes()));
+//            queryResult.setMimeType(StandardMimeType.CSV.toString());
 
         } catch (IOException e) {
             log.error(e);
             throw new SubmitExecutionFailedException(HTTPProvider.class.getName(), HTTPProvider.VERSION, e);
+        } catch (Exception e) {
+            log.error(e);
+            throw new QueryExecutionFailedException(HTTPProvider.class.getName(), HTTPProvider.VERSION);
         }
 
-        return queryResult;
+        return result;
     }
 
     @Override
