8cf65c8d8066c4ef5374b31d70980f6350673af2
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2019-07-08 11:37:04 -0400

    Update shutdown.sh

--- distribution/build-extras/bin/shutdown.sh
+++ distribution/build-extras/bin/shutdown.sh
@@ -1,4 +1,4 @@
 # stop daemon    
-pid=`ps uax | grep BINDAAS_INSTANCE | head -n 1 | awk '{print $2}'`   
+pid=`ps uax | grep BINDAAS_INSTANCE | head -n 1 | awk '{print $1}'`   
 echo "Killing $pid"
 kill -9 $pid
