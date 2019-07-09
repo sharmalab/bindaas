81fa95b41c93644d3ba3979eef525a16559a014b
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2019-07-09 11:00:59 -0400

    check os

--- distribution/build-extras/bin/shutdown.sh
+++ distribution/build-extras/bin/shutdown.sh
@@ -1,4 +1,9 @@
 # stop daemon    
-pid=`ps uax | grep BINDAAS_INSTANCE | head -n 1 | awk '{print $2}'`   
+if [ `which apk` ]; then
+   pid=`ps uax | grep BINDAAS_INSTANCE | head -n 1 | awk '{print $1}'`
+else
+   pid=`ps uax | grep BINDAAS_INSTANCE | head -n 1 | awk '{print $2}'`   
+fi
+
 echo "Killing $pid"
 kill -9 $pid
